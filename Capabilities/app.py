311e8438eb1bc9cfb451a01ec88b0b584a6af5b2
kentwkwong <kentwkwong@gmail.com>
2024-03-27 22:06:44 -0400

    first implement Cognito

--- Capabilities/app.py
+++ Capabilities/app.py
@@ -1,12 +1,58 @@
-from chalice import Chalice
-
+from chalice import Chalice, Response
+from chalice import CognitoUserPoolAuthorizer
 app = Chalice(app_name='Capabilities')
+
+authorizer = CognitoUserPoolAuthorizer(
+    'MyPool', provider_arns=['arn:aws:cognito-idp:ca-central-1:905418377930:userpool/ca-central-1_p1MezIG2a'],
+    scopes=["https://www.youtube.com/"])
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    with open('../Website/index.html','r') as f:
+        html_content = f.read()
+    return Response(
+        body=html_content,
+        status_code=200,
+        headers={'Content-Type': 'text/html'}
+    )
 
+@app.route('/loggedin')
+def loggedin():
+    with open('../Website/logged_in.html','r') as f:
+        html_content = f.read()
+    return Response(
+        body=html_content,
+        status_code=200,
+        headers={'Content-Type': 'text/html'}
+    )
+
+@app.route('/loggedout')
+def loggedout():
+    with open('../Website/logged_out.html','r') as f:
+        html_content = f.read()
+    return Response(
+        body=html_content,
+        status_code=200,
+        headers={'Content-Type': 'text/html'}
+    )
+
+@app.route('/user-pools', methods=['GET'], authorizer=authorizer)
+def authenticated():
+    return {"success": True}
+
+
+@app.route('/stockcode')
+def index():
+    # obj = webscrape_service()
+    # articles = obj.get_content()
+    # cleaning = datacleaning_service(articles)
+    # dict = cleaing.data_cleanup(cleaning)
+    # comp = comprehend_serivce(dict)
+    # dict = comp.get_score(dict)
+    print()
+    # return json{} # depends what information FE needs
+    return {"key": "abc", "value":"def123"}
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
