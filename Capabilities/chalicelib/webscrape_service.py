6d0351b475b592d0329c01fae94f1511185c466a
ThomasWongHY <hoyinwongccacca@gmail.com>
2024-04-01 10:56:46 -0400

    Add files via upload

--- Capabilities/chalicelib/webscrape_service.py
+++ Capabilities/chalicelib/webscrape_service.py
@@ -1,20 +1,26 @@
 from . import newsdata_service as newsdata
 import boto3
 
-def get_content(stockcode):
-    articles = newsdata.get_contents(stockcode)
-    # print(articles)
-    # result = '+ve|-ve|neutral'
-    # print(result)
-    # do Comprehend here
+def comprehend(text):
     client = boto3.client('comprehend')
-    response = client.detect_sentiment(Text=articles[0], LanguageCode='en')
+    response = client.detect_sentiment(Text=text, LanguageCode='en')
     sentiment_score = response['SentimentScore']
-    if sentiment_score['Positive'] > sentiment_score['Negative']:
+    if sentiment_score['Positive'] > 0.8:
         result = "positive"
-    elif sentiment_score['Positive'] < sentiment_score['Negative']:
+    elif sentiment_score['Negative'] > 0.8:
         result = "negative"
     else:
         result = "neutral"
+    # print(result)
+    return result
 
+def get_content(stockcode):
+    articles = newsdata.get_contents(stockcode)
+    print(articles)
+    # result = '+ve|-ve|neutral'
+    # print(result)
+    # do Comprehend here
+    # for a in articles:
+    #     result = comprehend(a)
+    result = comprehend(articles)
     return {'message':result}
